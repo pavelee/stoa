1d32f391f7c52cd06ccf7a963b0fc4366df29089
Paweł Ciosek <ciosek.pawel@gmail.com>
2022-09-01 00:09:04 +0200

    avatar design improvements

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -9,8 +9,8 @@ export const Avatar: FunctionComponent<{ user: User, size: number }> = ({ user, 
             <div className={`bg-neutral-focus text-neutral-content rounded-full w-${avataSize} h-${avataSize}`}>
                 {/* <span className="text-xl">{text}</span> */}
                 {
-                    user && <div className="relative">
-                        <img className="rounded-tl-xl rounded-tr-xl" src={user.avatar} />
+                    user && <div className="shadow-sm">
+                        <img className="rounded-tl-xl border border-yellow-500 rounded-tr-xl" src={user.avatar} />
                         {/* <div className="absolute bottom-0 text-white bg-yellow-500 rounded-full text-center">425</div> */}
                         <div className="text-white bg-yellow-500 text-center rounded-bl-xl rounded-br-xl">{user.points}</div>
                     </div>
